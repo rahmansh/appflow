95de0489809e8836832e21b6d194a7a3d0ed8cbc
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2026-06-04 00:55:31 +0200

    initial setup: server entry point

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import jobRoutes from './routes/jobs.js';
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json());
+
+
+app.use('/api/jobs', jobRoutes);
+
+app.get("/", (req, res) => res.send('AppFlow API running'));
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected');
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => console.error(err))
