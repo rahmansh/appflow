d71b430eac6788d23e139d668b252fac010cd31a
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2026-06-13 03:27:58 +0200

    setup vite react frontend with tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-8">
+        <h1 className="text-3xl font-bold">AppFlow works!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
