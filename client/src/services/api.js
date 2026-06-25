23c064c10e4558015aa21d567efe57c801d17490
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2026-06-25 11:33:55 +0200

    add api.js service and install axios

---
+++ client/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
+});
+
+
+export const getJobs = () => API.get('/api/jobs');
+export const createJob = (data) => API.post('/api/jobs', data);
+export const updateJob = (id, data) => API.put(`/api/jobs/${id}`, data);
+export const deleteJob = (id) => API.delete(`/api/jobs/${id}`);
